bafc7a394b51c1ac15e8c67b2b00e1b12ec8bfd3
oamer6 <poohd1ni@yahoo.com>
2023-01-23 19:52:55 -0500

    Added playermanager database via postgres

---
+++ src/main/java/com/example/playermanager/model/Player.java
@@ -0,0 +1,88 @@
+package com.example.playermanager.model;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Player implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String position;
+    private String phoneNumber;
+    private String imageUrl;
+    public Player() {}
+    public Player(String name, String email, String position, String phoneNumber, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", position='" + position + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
